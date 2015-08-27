6a4d861a2cff23403d66a09957989c9f9aae1e9b
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2015-08-27 08:56:34 -0400

    Render custom error page with rescue_from rather than exceptions_app.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+  rescue_from StandardError do |exception|
+    Honeybadger.notify(exception)
+
+    render "errors/show"
+  end
+
   def index
     raise "ka-friggin-boom"
   end
